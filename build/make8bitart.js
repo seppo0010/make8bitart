c5053e1667c0bc864ea23c39747d302e5b560db6
Sebastian Waisbrot <seppo0010@gmail.com>
2015-01-04 18:03:50 -0800

    Allow image uploads

--- build/make8bitart.js
+++ build/make8bitart.js
@@ -515,6 +515,7 @@ $(function() {
     $toolbox : $('#toolbox'),
     $savebox : $('#savebox'),
     $colorbox : $('#colorbox'),
+    $uploader : $('#uploader'),
     $waiting : $('#wait'),
 
     $tabs : $('.tabs'),
@@ -541,6 +542,9 @@ $(function() {
         
     $pixelSizeInput : $('.pixel-size-input'),
     $pixelSizeDemoDiv : $('#pixel-size-demo'),
+
+    $imageWidthInput : $('.uploader-width-input'),
+    $dropHere : $('.drop-here'),
     
     $minimizedToolsList : $('#minimized-tools-list'),
     $draggydivs : $('.draggy'),
@@ -637,6 +641,10 @@ $(function() {
   DOM.$toolbox.css({
     left : '30px',
     top : '120px'
+  });
+  DOM.$uploader.css({
+    left : '990px',
+    top : '50px'
   });
   DOM.$colorbox.css({
     left : '750px',
@@ -1292,6 +1300,87 @@ $(function() {
     }
   };
     
+
+  var onDragOver = function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'copy';
+  };
+
+  var onDrop = function(e) {
+    var palette = colorHistory;
+    if (palette.length === 0) {
+      // these are the default colors available in the picker
+      palette = ['ff0000', 'ffa500', 'ffff00', '008000', '0000ff', '800080', '000000', '808080', 'ffffff'];
+    }
+    console.log(palette);
+    var colors = [];
+    for (var c in palette) {
+      var color = palette[c];
+      colors.push([
+        parseInt(color[0] + color[1], 16),
+        parseInt(color[2] + color[3], 16),
+        parseInt(color[4] + color[5], 16)
+      ]);
+    }
+    var findColor = function(data) {
+      var best_match = null;
+      var best_match_distance = 0;
+      for (var i in colors) {
+        var color = colors[i];
+        var distance = (color[0] - data[0]) * (color[0] - data[0]) +
+          (color[1] - data[1]) * (color[1] - data[1]) +
+          (color[2] - data[2]) * (color[2] - data[2]);
+        if (best_match === null || best_match_distance > distance) {
+          best_match = palette[i];
+          best_match_distance = distance;
+        }
+      }
+      return best_match;
+    };
+
+    e.stopPropagation();
+    e.preventDefault();
+    var files = e.dataTransfer.files;
+    if (files.length > 1) {
+      alert('Use only one file, please');
+      return;
+    }
+    var file = files[0];
+    if (!file.type.match(/image.*/)) {
+      alert('File must be an image');
+      return;
+    }
+
+    var reader = new FileReader();
+
+    reader.onerror = function(e) {
+      alert('Error code: ' + e.target.error.code);
+    };
+
+    reader.onload = (function(aFile) {
+      return function(evt) {
+        var image = new Image();
+        var w = DOM.$imageWidthInput.val();
+        image.onload = function() {
+          var canvas = document.createElement('canvas');
+          new thumbnailer(canvas, image, w, 3);
+          var context = canvas.getContext('2d');
+          var h = canvas.height;
+          for (var i = 0; i < h; i++) {
+            for (var j = 0; j < w; j++) {
+              var data = context.getImageData(j, i, 1, 1).data;
+              var color = findColor(data);
+              drawPixel(j * pixel.size, i * pixel.size, hexToRgba(color), pixel.size);
+            }
+          }
+        };
+        image.src = evt.target.result;
+      };
+    })(file);
+
+    reader.readAsDataURL(file);
+  };
   /* tools */
   
   // draw clicked
@@ -1659,6 +1748,10 @@ $(function() {
       alert('Your browser doesn\'t support FileReader, which is required for uploading custom palettes.');
     }
   });
+
+  DOM.$imageWidthInput.change(function() {
+    DOM.$imageWidthInput.val($(this).val());
+  });
   
   
   
@@ -1701,4 +1794,107 @@ $(function() {
   DOM.$canvas[0].addEventListener('touchend', onMouseUp, false);
   DOM.$overlay[0].addEventListener('touchstart', onMouseDown, false);
   DOM.$overlay[0].addEventListener('touchend', onMouseUp, false);
+
+  DOM.$dropHere[0].addEventListener('dragover', onDragOver, false);
+  DOM.$dropHere[0].addEventListener('drop', onDrop, false);
+
+  // Source: http://stackoverflow.com/questions/2303690/resizing-an-image-in-an-html5-canvas#3223466
+  function lanczosCreate(lobes){
+    return function(x){
+      if (x > lobes)
+        return 0;
+      x *= Math.PI;
+      if (Math.abs(x) < 1e-16)
+        return 1;
+      var xx = x / lobes;
+      return Math.sin(x) * Math.sin(xx) / x / xx;
+    };
+  }
+
+  //elem: canvas element, img: image element, sx: scaled width, lobes: kernel radius
+  function thumbnailer(elem, img, sx, lobes) {
+    this.canvas = elem;
+    elem.width = img.width;
+    elem.height = img.height;
+    elem.style.display = "none";
+    this.ctx = elem.getContext("2d");
+    this.ctx.drawImage(img, 0, 0);
+    this.img = img;
+    this.src = this.ctx.getImageData(0, 0, img.width, img.height);
+    this.dest = {
+      width: sx,
+      height: Math.round(img.height * sx / img.width),
+    };
+    this.dest.data = new Array(this.dest.width * this.dest.height * 3);
+    this.lanczos = lanczosCreate(lobes);
+    this.ratio = img.width / sx;
+    this.rcp_ratio = 2 / this.ratio;
+    this.range2 = Math.ceil(this.ratio * lobes / 2);
+    this.cacheLanc = {};
+    this.center = {};
+    this.icenter = {};
+    this.process1(0);
+  }
+
+  thumbnailer.prototype.process1 = function(u) {
+    var idx;
+    this.center.x = (u + 0.5) * this.ratio;
+    this.icenter.x = Math.floor(this.center.x);
+    for (var v = 0; v < this.dest.height; v++) {
+      this.center.y = (v + 0.5) * this.ratio;
+      this.icenter.y = Math.floor(this.center.y);
+      var a, r, g, b;
+      a = r = g = b = 0;
+      for (var i = this.icenter.x - this.range2; i <= this.icenter.x + this.range2; i++) {
+        if (i < 0 || i >= this.src.width)
+          continue;
+        var f_x = Math.floor(1000 * Math.abs(i - this.center.x));
+        if (!this.cacheLanc[f_x])
+          this.cacheLanc[f_x] = {};
+        for (var j = this.icenter.y - this.range2; j <= this.icenter.y + this.range2; j++) {
+          if (j < 0 || j >= this.src.height)
+            continue;
+          var f_y = Math.floor(1000 * Math.abs(j - this.center.y));
+          if (this.cacheLanc[f_x][f_y] === undefined)
+            this.cacheLanc[f_x][f_y] = this.lanczos(Math.sqrt(Math.pow(f_x * this.rcp_ratio, 2) + Math.pow(f_y * this.rcp_ratio, 2)) / 1000);
+          weight = this.cacheLanc[f_x][f_y];
+          if (weight > 0) {
+            idx = (j * this.src.width + i) * 4;
+            a += weight;
+            r += weight * this.src.data[idx];
+            g += weight * this.src.data[idx + 1];
+            b += weight * this.src.data[idx + 2];
+          }
+        }
+      }
+      idx = (v * this.dest.width + u) * 3;
+      this.dest.data[idx] = r / a;
+      this.dest.data[idx + 1] = g / a;
+      this.dest.data[idx + 2] = b / a;
+    }
+
+    if (++u < this.dest.width)
+      this.process1(u);
+    else
+      this.process2();
+  };
+
+  thumbnailer.prototype.process2 = function(){
+    this.canvas.width = this.dest.width;
+    this.canvas.height = this.dest.height;
+    this.ctx.drawImage(this.img, 0, 0);
+    this.src = this.ctx.getImageData(0, 0, this.dest.width, this.dest.height);
+    var idx, idx2;
+    for (var i = 0; i < this.dest.width; i++) {
+      for (var j = 0; j < this.dest.height; j++) {
+        idx = (j * this.dest.width + i) * 3;
+        idx2 = (j * this.dest.width + i) * 4;
+        this.src.data[idx2] = this.dest.data[idx];
+        this.src.data[idx2 + 1] = this.dest.data[idx + 1];
+        this.src.data[idx2 + 2] = this.dest.data[idx + 2];
+      }
+    }
+    this.ctx.putImageData(this.src, 0, 0);
+    this.canvas.style.display = "block";
+  };
 });
